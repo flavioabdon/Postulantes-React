c1f63b2e0a34f03ed7d4fffa3f5d62ba3fc684e1
flavio abdon <flaviocondori@gmail.com>
2025-11-24 10:29:52 -0400

    Add allowedHosts to Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
+    host: '0.0.0.0',
     port: 3000,
+    allowedHosts: [
+      "convocatoriaelecciones.duckdns.org",
+    ],
   },
 });
