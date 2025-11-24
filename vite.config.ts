163a163681698686bd8d3e707e2b5844bade0c9b
flavio abdon <flaviocondori@gmail.com>
2025-11-24 13:18:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: [
-      "serecipeb-subnacionales.duckdns.org",
-    ],
+
+    // IMPORTANTE: allowedHosts solo para Vite 6+
+    allowedHosts: ['serecipeb-subnacionales.duckdns.org'],
+
     hmr: {
       protocol: 'wss',
       host: 'serecipeb-subnacionales.duckdns.org',
-      port: 443
-    }    
-  },
+      port: 443,
+      clientPort: 443          // ⭐ Evita fallback a localhost:3000
+    }
+  }
 });
